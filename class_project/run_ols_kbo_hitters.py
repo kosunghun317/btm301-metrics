eeb108a31fb47b04e34636bc0df8b74558309b15
kosunghun317 <kosunghun317@gmail.com>
2024-12-11 17:57:57 +0900

    added rest cases

---
+++ class_project/run_ols_kbo_hitters.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.stats.diagnostic import het_breuschpagan
+from statsmodels.stats.stattools import durbin_watson
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+raw_data = pd.read_csv("./class_project/data/csv/kbo_hitters.csv")
+
+data = raw_data.copy()
+data.dropna(inplace=True)
+data.rename(columns={"Age 7/1/24": "Age"}, inplace=True)
+
+cpi = {
+    2010: 90.0,
+    2011: 94.0,
+    2012: 96.0,
+    2013: 97.0,
+    2014: 98.0,
+    2015: 99.0,
+    2016: 100.0,
+    2017: 101.0,
+    2018: 103.0,
+    2019: 104.0,
+    2020: 105.0,
+}
+
+
+data["cpi"] = data["year"].map(cpi)
+data["cpi_rate"] = data["cpi"] / cpi[2010]
+
+# model is similar to Cobb-Douglas production function
+data["AAV"] = np.log(data["AAV"] / data["cpi_rate"])
+data["Win_Pct"] = np.log(data["Win_Pct"])
+data["Attendance"] = np.log(data["Attendance"])
+data["Age"] = np.log(data["Age"])
+
+columns_to_check = ["OBP", "SLG"]
+data = data.loc[~(data[columns_to_check] == 0).any(axis=1)]
+data["OBP"] = np.log(data["OBP"])
+data["SLG"] = np.log(data["SLG"])
+data["HR"] = np.log(data["HR"] + 1)
+data["PA"] = np.log(data["PA"] + 1)
+data["OBP_league"] = np.log(data["OBP_league"])
+data["SLG_league"] = np.log(data["SLG_league"])
+
+# drop unnecessary columns
+data.drop(
+    ["Player", "cpi", "year", "cpi_rate", "Old Club", "New Club"], axis=1, inplace=True
+)
+
+
+# regression
+X = data.drop("AAV", axis=1)
+X = sm.add_constant(X)
+y = data["AAV"]
+
+model = sm.OLS(y, X)
+results = model.fit()
+
+print(results.summary())
+
+# Residuals and fitted values
+residuals = results.resid
+fitted = results.fittedvalues
+
+# Plot actual vs fitted
+plt.figure(figsize=(8, 6))
+plt.scatter(y, fitted)
+plt.xlabel("Actual AAV")
+plt.ylabel("Fitted AAV")
+plt.title("Scatter Plot of Actual vs. Fitted AAV")
+plt.savefig("./class_project/images/kbo_hitters_fitted.png")
+plt.show()
+
+# plot residuals versus independent variables
+num_vars = len(X.columns) - 1
+cols = 4
+rows = (num_vars // cols) + (num_vars % cols > 0)
+
+fig, axes = plt.subplots(rows, cols, figsize=(15, rows * 4))
+axes = axes.flatten()
+
+for i, col in enumerate(X.columns[1:]):
+    sns.scatterplot(x=data[col], y=residuals, ax=axes[i])
+    axes[i].axhline(0, color="red", linestyle="--", linewidth=1)
+    axes[i].set_title(f"Residuals vs. {col}")
+    axes[i].set_xlabel(col)
+    axes[i].set_ylabel("Residuals")
+
+for j in range(i + 1, len(axes)):
+    fig.delaxes(axes[j])
+
+plt.tight_layout()
+plt.savefig("./class_project/images/kbo_hitters_residuals.png")
+plt.show()
---
+++ class_project/run_ols_mlb_hitters.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.stats.diagnostic import het_breuschpagan
+from statsmodels.stats.stattools import durbin_watson
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+raw_data = pd.read_csv("./class_project/data/csv/mlb_hitters.csv")
+
+data = raw_data.copy()
+data.dropna(inplace=True)
+data.rename(columns={"Age 7/1/24": "Age"}, inplace=True)
+
+cpi = {
+    2010: 218.056,
+    2011: 224.939,
+    2012: 229.594,
+    2013: 232.957,
+    2014: 236.736,
+    2015: 237.017,
+    2016: 240.007,
+    2017: 245.120,
+    2018: 251.107,
+    2019: 255.657,
+    2020: 258.811,
+}
+
+# adjust for inflation
+data["cpi"] = data["year"].map(cpi)
+data["cpi_rate"] = data["cpi"] / cpi[2010]
+
+# model is similar to Cobb-Douglas production function
+data["AAV"] = np.log(data["AAV"] / data["cpi_rate"])
+data["Win_Pct"] = np.log(data["Win_Pct"])
+data["Attendance"] = np.log(data["Attendance"])
+data["Age"] = np.log(data["Age"])
+data["New_Team_Payroll_Prev_Year"] = np.log(
+    data["New_Team_Payroll_Prev_Year"] / data["cpi_rate"]
+)
+columns_to_check = ["OBP", "SLG"]
+data = data.loc[~(data[columns_to_check] == 0).any(axis=1)]
+data["OBP"] = np.log(data["OBP"])
+data["SLG"] = np.log(data["SLG"])
+data["HR"] = np.log(data["HR"] + 1)
+data["PA"] = np.log(data["PA"] + 1)
+data["OBP_league"] = np.log(data["OBP_league"])
+data["SLG_league"] = np.log(data["SLG_league"])
+
+# drop unnecessary columns
+data.drop(
+    ["Player", "cpi", "year", "cpi_rate", "Old Club", "New Club"], axis=1, inplace=True
+)
+
+
+# regression
+X = data.drop("AAV", axis=1)
+X = sm.add_constant(X)
+y = data["AAV"]
+
+model = sm.OLS(y, X)
+results = model.fit()
+
+print(results.summary())
+
+# Residuals and fitted values
+residuals = results.resid
+fitted = results.fittedvalues
+
+# Plot actual vs fitted
+plt.figure(figsize=(8, 6))
+plt.scatter(y, fitted)
+plt.xlabel("Actual AAV")
+plt.ylabel("Fitted AAV")
+plt.title("Scatter Plot of Actual vs. Fitted AAV")
+plt.savefig("./class_project/images/mlb_hitters_fitted.png")
+plt.show()
+
+# plot residuals versus independent variables
+num_vars = len(X.columns) - 1
+cols = 4
+rows = (num_vars // cols) + (num_vars % cols > 0)
+
+fig, axes = plt.subplots(rows, cols, figsize=(15, rows * 4))
+axes = axes.flatten()
+
+for i, col in enumerate(X.columns[1:]):
+    sns.scatterplot(x=data[col], y=residuals, ax=axes[i])
+    axes[i].axhline(0, color="red", linestyle="--", linewidth=1)
+    axes[i].set_title(f"Residuals vs. {col}")
+    axes[i].set_xlabel(col)
+    axes[i].set_ylabel("Residuals")
+
+for j in range(i + 1, len(axes)):
+    fig.delaxes(axes[j])
+
+plt.tight_layout()
+plt.savefig("./class_project/images/mlb_hitters_residuals.png")
+plt.show()
